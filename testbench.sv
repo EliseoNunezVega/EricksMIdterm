64a7e3b1d8adbc6c98db1ac7c30fa2e53511d9cb
jpeezzy <jpeezzy@hotmail.com>
2016-11-27 15:54:44 -0800

    Update testbench.sv

--- testbench.sv
+++ testbench.sv
@@ -1,1 +1,19 @@
+module testbench;
 
+    logic clk;
+    logic reset;     
+processor_v1(
+        .clk(clk),
+        .clkreset(reset)
+    );
+    genvar i; 
+        begin
+       for (i = 0; i<31; i++)begin
+    end
+    end
+initial begin
+repeat(10) begin
+end
+end
+
+endmodule    
